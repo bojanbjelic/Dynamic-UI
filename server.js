72ad39fff5653807cc44ed84e21b3da36b429d90
Paulo Genargue <paulo.genargue@gmail.com>
2016-11-24 00:55:45 +0800

    Added lightweight file storage

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var uuid = require('uuid');
 var moment = require('moment');
+var datastore = require('nedb')
 
 app.set('views', './views');
 app.set('view engine', 'pug');
@@ -10,6 +11,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.locals.moment = moment;
+
+var db = {};
+db.container = new datastore({
+   filename: __dirname + 'db/container.db',
+   autoload: true
+});
+db.metadata = new datastore({
+   filename: __dirname + 'db/metadata.db',
+   autoload: true
+});
+
+console.log('db at ' + __dirname);
 
 // Temporary storage in memory
 var _containers = [];
@@ -24,10 +37,16 @@ app.get('/', function (req, res) {
  * Metadata
  */
 app.get('/metadata', function(req, res){
-  if (req.accepts('html'))
-     res.render('metadata', {metadata: _metadata});
-  else
-    res.send(_metadata);
+    db.metadata.find({}, (error, metadata)=>{
+      if (!error){
+        if (req.accepts('html'))
+          res.render('metadata', {metadata: metadata});
+        else
+          res.send(metadata);
+      }else{
+        res.status(500).send({error: error});
+      }
+    });
 });
 
 app.get('/metadata/new', function(req, res){
@@ -37,21 +56,29 @@ app.get('/metadata/new', function(req, res){
 
 app.post('/metadata', function(req, res){
   var data = req.body;
-  data.id = uuid.v4();
+  // data.id = uuid.v4();
   data.createdAt = new Date();
-  _metadata.push(data);
-  res.sendStatus(201);
+
+  db.metadata.insert(data, (error, newMetadata)=>{
+    if (!error)
+      res.json(newMetadata);
+    else
+      res.status(500).send({error: error});
+  })
+  // _metadata.push(data);
+  // res.sendStatus(201);
 });
 
 /*
  * Form
  */
 app.get('/form/:id', function(req, res){
-  var found = _metadata.find(c => c.id == req.params.id);
-  if (found)
-    res.render('form', {metadata: found});
-  else
-    res.sendStatus(404);
+  db.metadata.findOne({_id: req.params.id}, (error, found)=>{
+    if (found)
+      res.render('form', {metadata: found});
+    else
+      res.sendStatus(404);
+  });
 });
 
 /*
@@ -68,7 +95,9 @@ app.post('/container', function(req, res){
 });
 
 app.get('/container/new', function(req, res){
-  res.render('new_container', { metadataList: _metadata});
+  db.metadata.find({}, (error, list)=>{
+    res.render('new_container', { metadata: list});
+  });
 });
 
 /*
